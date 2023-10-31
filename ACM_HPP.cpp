02b758b5e19dbfabb40229e15229f62280436c2b
Vinícius de Oliveira Marcos <115245103+viniciusomarcos@users.noreply.github.com>
2023-10-31 13:46:56 -0300

    Update ACM_HPP.cpp

--- ACM_HPP.cpp
+++ ACM_HPP.cpp
@@ -2,6 +2,7 @@
 #define ACM_HPP
 #include <iostream>
 #include <vector>
+#include <array>
 #include <string>
 
 using namespace std;
@@ -9,15 +10,15 @@ using namespace std;
 class Grafo {
 private:
     int numVertices;
-    vector<vector<int>> matrizAdj;
+    vector<vector<double>> matrizAdj;
 
 public:
     Grafo(int n);
-    void adicionarAresta(int origem, int destino, int custo);
+    void adicionarAresta3(double origem, double destino, double custo); //usado na versão 3 - atualização do algortmo de Prim
     void imprimirGrafo();
-    int MenorCusto(int vertice, Grafo sol, vector<bool>& marcador, int option);
-    int gerarCustoAleatorio(int valorMaximo);
-    Grafo AMC(int num_vet, int capacidade);
-}
+    int CalculaCapacidade(int raiz, int antecessor); // usado na versão 3 - atualização do algortmo de Prim
+    array<double, 3> MenorCusto3(int raiz, Grafo sol, vector<bool>& marcador, int capacidade); // usado na versão 3 - atualização do algortmo de Prim
+    Grafo AMC3(int num_vet, int raiz, vector<int>& capacidade); // usado na versão 3 - atualização do algortmo de Prim
+};
 
 #endif
